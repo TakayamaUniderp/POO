991f867ae9e4ce6d1a510b6c982dbe75ad755367
TakayamaUniderp <142752874+TakayamaUniderp@users.noreply.github.com>
2023-09-17 19:29:21 -0300

    rascunho?

---
+++ lista005/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    Aviao a1 = new Aviao("12" , "Boeing", "727JK", "BRANCO", 2011, 2012, "70T", "75T", 89, 4, "Boeing", 23000, "GASOLINA", "GOL LTDA", "AÉREO", 
+    95, 2, "P1231");
+
+    a1.imprimir();
+
+    }
+}
---
+++ lista005/src/BaseId.java
@@ -0,0 +1,17 @@
+abstract class BaseId {
+   
+    protected String id;
+
+    public BaseId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+}
